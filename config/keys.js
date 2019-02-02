a279d9d97f2698f176d85c7f29ddfd93ed4971e2
abed-taki <pay2taki@gmail.com>
2019-02-02 17:35:23 +0100

    ready to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://abed:Abedaness1@ds211088.mlab.com:11088/recipury",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
