a736ca0bc4bdb6079d3fd414cc3e1546b7bea8f1
abed-taki <pay2taki@gmail.com>
2019-02-03 17:13:14 +0100

    final

---
+++ client/build/precache-manifest.61634fcc79c79c9ce52fdefad99ceb11.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "d33d91ce2fae411ce161bdc09f659801",
+    "url": "/static/media/spinner.d33d91ce.gif"
+  },
+  {
+    "revision": "4d012b23d175eddf0d12",
+    "url": "/static/css/main.cb154f09.chunk.css"
+  },
+  {
+    "revision": "b68ad81aab53156a6e07",
+    "url": "/static/js/1.b68ad81a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d86903d35a2cb2cfd9203f88d5c8e1d",
+    "url": "/static/media/twitter.5d86903d.svg"
+  },
+  {
+    "revision": "83c8f90acfb1059cbffb933487e584bf",
+    "url": "/static/media/instagram.83c8f90a.svg"
+  },
+  {
+    "revision": "8ce183c9105f811ffec3db4d6249b966",
+    "url": "/static/media/facebook.8ce183c9.svg"
+  },
+  {
+    "revision": "87fa4fc6e51571cfa309a045e532cd9a",
+    "url": "/static/media/youtube.87fa4fc6.svg"
+  },
+  {
+    "revision": "7d2512bc8ddc379de07f1776e735f61b",
+    "url": "/static/media/behance.7d2512bc.svg"
+  },
+  {
+    "revision": "4d012b23d175eddf0d12",
+    "url": "/static/js/main.4d012b23.chunk.js"
+  },
+  {
+    "revision": "34ebdadb5bf751aa282098c98d2eaec4",
+    "url": "/static/media/cook.34ebdadb.svg"
+  },
+  {
+    "revision": "1d30b878fdf2b27c8360c4fce475de4d",
+    "url": "/static/media/login.1d30b878.jpg"
+  },
+  {
+    "revision": "e93ca17257cc67156a125189a59e5def",
+    "url": "/static/media/register.e93ca172.jpg"
+  },
+  {
+    "revision": "30d8dedc7fee6314df3a555ea8b6bb0c",
+    "url": "/static/media/heart.30d8dedc.svg"
+  },
+  {
+    "revision": "f7e16553320a96ff1d6ac1a583b8e0c3",
+    "url": "/static/media/heart-red.f7e16553.svg"
+  },
+  {
+    "revision": "9233260567a8f1d863e4ce15f25582d2",
+    "url": "/static/media/footer.92332605.jpg"
+  },
+  {
+    "revision": "e2bc5309a595356a661f492f2f5aeb3c",
+    "url": "/static/media/cover.e2bc5309.jpg"
+  },
+  {
+    "revision": "3db3ade89ab5e4e068766a2626846201",
+    "url": "/static/media/bg.3db3ade8.jpg"
+  },
+  {
+    "revision": "5b88cf11aa3f334b87f709164ca6d706",
+    "url": "/index.html"
+  }
+];
